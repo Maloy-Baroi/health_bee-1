85a7e8d242d3e0c1a79daf566df060e7603c36b3
unknown <maloy.virat@gmail.com>
2023-05-18 22:59:16 +0600

    Added ServiceCart Model, Updated Appointment Model, and Create User Home Data

--- health_bee-backend/App_main/views.py
+++ health_bee-backend/App_main/views.py
@@ -1,24 +1,27 @@
 from rest_framework import viewsets, permissions, generics
 from rest_framework.response import Response
 from rest_framework.decorators import action
+from rest_framework.views import APIView
 
 from App_main.models import *
 from App_main.serializers import *
+from django.db.models import Sum
 
 
-class ProfileExistOrNot(viewsets.ModelViewSet):
-    queryset = PatientProfile.objects.all()
-    serializer_class = PatientProfileSerializer
+class UserHomeData(APIView):
     permission_classes = [permissions.IsAuthenticated]
 
-    @action(detail=False, methods=['get'])
-    def check_existence(self, request):
-        user = request.user
-        try:
-            profile = self.queryset.get(user=user)
-            return Response({"profile": "True"})
-        except PatientProfile.DoesNotExist:
-            return Response({"profile": "False"})
+    def get(self, request, format=None):
+        profile_exists_or_not = PatientProfile.objects.filter(user=request.user).exists()
+
+        appointments_running = Appointment.objects.filter(user=request.user, status="Scheduled").count()
+        appointments_completed = Appointment.objects.filter(user=request.user, status="Completed").count()
+
+        total_tests = Appointment.objects.filter(user=request.user, status='Completed').aggregate(Sum('service__services'))
+        total_tests_count = total_tests['service__services__sum'] or 0
+
+        return Response({"profile": f"{profile_exists_or_not}", "appointment_running": appointments_running, 'appointment_completed': appointments_completed, 'total_complted': appointments_completed, 'total_tests': f"{total_tests_count}"})
+
 
 class PatientProfileViewSet(viewsets.ModelViewSet):
     queryset = PatientProfile.objects.all()
